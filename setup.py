eecc8da40763f0f6bcfa7c24dbc4fbaf22f7335b
Quentin <quentin.guidee@gmail.com>
2019-10-30 18:32:51 +0100

    Ready to pipy

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='omega-cli',
+    version='0.1',
+    description='Omega installer for Numworks',
+    author='Quentin Guidée',
+    author_email='quentin.guidee@gmail.com',
+    url='https://github.com/Omega-Numworks/Omega-CLI-Installer/archive/omega-cli-0.1.tar.gz',
+    packages=['omega-cli'],
+    install_requires=[
+        'click',
+        'PyInquirer'
+    ],
+)
