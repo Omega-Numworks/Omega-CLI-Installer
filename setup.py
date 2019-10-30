7e69e457cc4cfc2178cf5df6d55cf3ccb32b636c
Quentin <quentin.guidee@gmail.com>
2019-10-30 18:46:06 +0100

    Fix bugs

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(name='omega-cli',
-    version='0.1.1',
+    version='0.1.2',
     description='Omega installer for Numworks',
     author='Quentin Guidée',
     author_email='quentin.guidee@gmail.com',
-    url='https://github.com/Omega-Numworks/Omega-CLI-Installer/archive/omega-cli-0.1.1.tar.gz',
+    url='https://github.com/Omega-Numworks/Omega-CLI-Installer/releases/download/0.1.2/omega-cli-0.1.2.tar.gz',
     packages=['omega-cli'],
     install_requires=[
         'click',
